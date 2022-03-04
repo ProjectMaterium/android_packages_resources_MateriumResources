df58841f754e6ffc69288b0a1c3dd7475b32288c
nift4 <nift4@protonmail.com>
2022-03-04 13:16:06 +0100

    pull-translations: refactor, add Settings

--- pull-translations.sh
+++ pull-translations.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
-cd "$ANDROID_BUILD_TOP"
 declare -A dirs
 TARGETPATH="$ANDROID_BUILD_TOP/packages/resources/MateriumTranslations"
 ###    Definitions     ###
 dirs["Backgrounds"]="packages/apps/Backgrounds/res"
+dirs["Settings"]="packages/apps/Settings/res"
 ### End of Definitions ###
 for i in "${!dirs[@]}"; do
-cd "${dirs[$i]}"
-cp values/materium_strings.xml "$TARGETPATH/$i.xml"
-cd "$ANDROID_BUILD_TOP"
+cp "$ANDROID_BUILD_TOP/${dirs[$i]}"/values/materium_strings.xml "$TARGETPATH/$i.xml"
 done
-cd "$TARGETPATH"
 git add --all
 git commit -m "[Automated] Pull translations"
