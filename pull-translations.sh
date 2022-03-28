a9b400e44113fee33041a4b275e854c229183ad6
nift4 <nift4@protonmail.com>
2022-03-28 16:54:48 +0200

    droid-ng-ify

--- pull-translations.sh
+++ pull-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 declare -A dirs
-TARGETPATH="$ANDROID_BUILD_TOP/packages/resources/MateriumTranslations"
+TARGETPATH="$ANDROID_BUILD_TOP/packages/resources/NgTranslations"
 ###    Definitions     ###
 dirs["Backgrounds"]="packages/apps/Backgrounds/res"
 dirs["Settings"]="packages/apps/Settings/res"
@@ -12,7 +12,7 @@ dirs["SettingsLib"]="frameworks/base/packages/SettingsLib/res"
 dirs["support"]="vendor/support/res"
 ### End of Definitions ###
 for i in "${!dirs[@]}"; do
-cp "$ANDROID_BUILD_TOP/${dirs[$i]}"/values/materium_strings.xml "$TARGETPATH/$i.xml"
+cp "$ANDROID_BUILD_TOP/${dirs[$i]}"/values/ng_strings.xml "$TARGETPATH/$i.xml"
 done
 git -C "$TARGETPATH" add .
 git -C "$TARGETPATH" commit -m "[Automated] Pull translations"
